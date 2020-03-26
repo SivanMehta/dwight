c8b5205f3d7ad390b4a9c7d93ce3a215b2cf80a3
Sivan Mehta <Sivanmehta2@gmail.com>
2020-03-25 18:59:17 -0700

    [ux] use relative performance

--- plot.R
+++ plot.R
@@ -3,10 +3,14 @@ library(dplyr)
 dat <- read.csv('data.csv')
 
 starting.worth = dat[1, ]$worth
+starting.price = dat[1, ]$price
 
 dat %>%
   mutate(cycles = as.numeric(rownames(dat))) %>%
+  mutate(relative.worth = worth / starting.worth) %>%
+  mutate(relative.price = price / starting.price) %>%
   ggplot() +
-    geom_line(aes(cycles, worth), color = 'green') +
-    geom_line(aes(cycles, price)) +
-    geom_hline(yintercept = starting.worth)
+    geom_line(aes(cycles, relative.worth), color = 'green') +
+    geom_hline(yintercept = 1) +
+    geom_line(aes(cycles, relative.price)) +
+    labs(y = "Relative Performance")
